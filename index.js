e7cd350b29f79d257fd6539431854255fcb436ac
Grawrb <robmaxfield22@gmail.com>
2024-08-19 15:34:39 -0600

    Second attempt at proper programming of svg generator. Simplified class constructors in 'shapes.js'. Fixed error in inquirer questions. Tested by creating logo with each shape.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Triangle, Square } = require('./lib/shapes');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
 // prompts for Inquirer
-function questions()  {
-    inquirer.prompt([
+const questions =  [
 
     {
+        message: "Enter three characters",
         type: "input",
         name: "text",
-        message: "Enter three characters",
+        
         // Function to validate that user entered exactly three characters, otherwise return error message
         validate: function (value) {
             if (value.length === 3) {
@@ -20,47 +20,48 @@ function questions()  {
         }
     },
     {
+        message: "Enter a color for the text to be used (you may use a color keyword or a hexidecimal number)",
         type: "input",
         name: "textColor",
-        message: "Enter a color for the text to be used (you may use a color keyword or a hexidecimal number)"
+        
     },
     {
+        message: "Select a shape for your logo",
         type: "list",
         name: "shape",
-        message: "Select a shape for your logo",
-        choices: ["Circle, Triangle, Square"]
+        choices: ['Circle', 'Triangle', 'Square'],
     },
     {
+        message: "Enter a color for your selected shape (you may use a color keyword or a hexidecimal number)",
         type: "input",
         name: "shapeColor",
-        message: "Enter a color for your selected shape (you may use a color keyword or a hexidecimal number)"
     },
-    ])
-    .then((answers) => {
-        console.log(answers);
-        const svg = generateSVG(answers);
-        fs.writeFile('logo.svg', svg, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        });
-    });
-}
-questions();
+];
 
-function generateSVG(answers) {
+// Runs inquirer and captures user answers. Takes user input and assigns selected shape and color to 'shape' variable.
+inquirer.prompt(questions).then((answers) => {
     let shape;
     switch (answers.shape) {
-        case "Circle":
-            shape = new Circle(answers.text, answers.textColor, answers.shapeColor);
+        case 'Triangle':
+            shape = new Triangle(answers.shapeColor);
             break;
-        case "Triangle":
-            shape = new Triangle(answers.text, answers.textColor, answers.shapeColor);
+        case 'Circle':
+            shape = new Circle(answers.shapeColor);
             break;
-        case "Square":
-            shape = new Square(answers.text, answers.textColor, answers.shapeColor);
+        case 'Square':
+            shape = new Square(answers.shapeColor);
             break;
     }
-    return shape.render();
-};
 
+    // Generates html of new svg. Uses user inputs for shape, text content, and text color
+    const svg = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+            ${shape.render()}
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+        </svg>
+    `;
+
+    fs.writeFileSync('logo.svg', svg.trim());
+    console.log('Generated logo.svg');
+});
 
